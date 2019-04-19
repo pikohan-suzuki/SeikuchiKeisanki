6e74112af3803c9b78fbaecbe4f82e308f83af8b
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-19 11:47:07 +0900

    フラグメントのアタッチ処理を追加

--- SeikuchiKeisanki/app/src/main/java/com/amebaownd/pikohan_niwatori/seikuchikeisanki/FooterMenuFragment.kt
+++ SeikuchiKeisanki/app/src/main/java/com/amebaownd/pikohan_niwatori/seikuchikeisanki/FooterMenuFragment.kt
@@ -7,13 +7,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class FooterMenuFragment : Fragment(){
+class FooterMenuFragment() : Fragment(){
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        
+        return inflater.inflate(R.layout.footer_menu_fragment,container,false)
+    }
+
+    fun createInstance(context:Context):FooterMenuFragment{
+        return FooterMenuFragment()
     }
 }
--- SeikuchiKeisanki/app/src/main/java/com/amebaownd/pikohan_niwatori/seikuchikeisanki/MainActivity.kt
+++ SeikuchiKeisanki/app/src/main/java/com/amebaownd/pikohan_niwatori/seikuchikeisanki/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.amebaownd.pikohan_niwatori.seikuchikeisanki
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v7.widget.LinearLayoutCompat
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.footer_layout,FooterMenuFragment::createInstance(this),"footer_menu_fragment")
+
     }
+
+
 }
