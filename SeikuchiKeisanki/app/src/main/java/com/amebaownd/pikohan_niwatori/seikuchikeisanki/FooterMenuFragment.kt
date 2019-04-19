c1642897219c007150bd5015390c652ce714dbd5
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-19 10:54:48 +0900

    フッターメニューレイアウトの作成

---
+++ SeikuchiKeisanki/app/src/main/java/com/amebaownd/pikohan_niwatori/seikuchikeisanki/FooterMenuFragment.kt
@@ -0,0 +1,19 @@
+package com.amebaownd.pikohan_niwatori.seikuchikeisanki
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FooterMenuFragment : Fragment(){
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        
+    }
+}
